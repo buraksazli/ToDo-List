0f2eda0bff9477e43c3252acd0371fcc8e05b5aa
Burak Sazlı <buraksazli0@gmail.com>
2023-12-15 20:22:05 +0300

    Task deletion added

--- script.js
+++ script.js
@@ -16,11 +16,6 @@ function addTask(e) {
     }
 }
 
-function deleteTask() {
-    
-        
-}
-
 function storeTasks(job) {
     let stored_tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     stored_tasks.push(job);
@@ -37,18 +32,33 @@ function listTasks() {
 function createNode(job) {
     const new_task = document.createElement('div');
     const new_text = document.createTextNode(job);
-    const new_button = document.createElement("button"); 
+    const new_delete_button = document.createElement("button"); 
     const new_icon = document.createElement("i");
-    
+    new_delete_button.addEventListener("click" , function() {
+        deleteTask(job);
+    });
     new_icon.classList += "fa-solid fa-trash fa-xs";
     new_task.classList += "border border-primary bg-primary text-white rounded p-2 mb-2 d-flex justify-content-between";
-    new_button.classList += "btn btn-danger";
+    new_delete_button.classList += "btn btn-danger delete";
         
     new_task.appendChild(new_text); 
-    new_button.appendChild(new_icon);
-    new_task.appendChild(new_button);
+    new_delete_button.appendChild(new_icon);
+    new_task.appendChild(new_delete_button);
     all_tasks.appendChild(new_task);     
 }    
+
+function deleteTask(job) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let index = tasks.indexOf(job);
+    tasks.splice(index , 1);
+    localStorage.setItem("tasks" , JSON.stringify(tasks));
+    let items = all_tasks.getElementsByTagName("div");
+    for(let i = 0 ; i < items.length ; i++) {
+        if(items[i].textContent == job) 
+            all_tasks.removeChild(items[i]);         
+    }
+}
+    
 
 
 
